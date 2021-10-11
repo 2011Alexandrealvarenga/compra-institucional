81761de639d84f8d7889e1e23dd8a5fa80d4d1b6
2011brunofernandes <2011brunofernandes@gmail.com>
2021-10-11 13:54:39 -0300

    add files for css and js

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classificados</title>
+    <link href="assets/css/bootstrasp.min.css" />
+    <link href="assets/css/style.css" />
+    
+    <script src="assets/js/jquery.min.js"> </script>
+    <script src="assets/js/bootstrap.min.js"> </script>
+    <script src="assets/js/script.min.js"> </script>
+    
+</head>
+<body>
+    
+</body>
+</html>
