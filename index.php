0e5b89a5eb23965fd1b5036149c4ccf190e3375d
2011brunofernandes <2011brunofernandes@gmail.com>
2021-10-11 14:06:14 -0300

    add header

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classificados</title>
-    <link href="assets/css/bootstrasp.min.css" />
-    <link href="assets/css/style.css" />
-    
-    <script src="assets/js/jquery.min.js"> </script>
-    <script src="assets/js/bootstrap.min.js"> </script>
-    <script src="assets/js/script.min.js"> </script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Classificados</title>
+	<link rel="stylesheet" href="assets/css/bootstrasp.min.css">
+	<link rel="stylesheet" href="assets/css/style.css" />
+	
+	<script src="assets/js/jquery.min.js"> </script>
+	<script src="assets/js/bootstrap.min.js"> </script>
+	<script src="assets/js/script.min.js"> </script>
     
 </head>
 <body>
-    
+  <nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="./" class="navbar-brand">Classificados</a>
+			</div>
+		</div>
+	</nav>
 </body>
 </html>
