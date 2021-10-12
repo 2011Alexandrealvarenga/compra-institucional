26ef7e3735511076f36bd64aae0280944170640f
2011brunofernandes <2011brunofernandes@gmail.com>
2021-10-12 15:29:52 -0300

    add anuncio - criacao do arquivo v2

--- add-anuncio.php
+++ add-anuncio.php
@@ -8,6 +8,37 @@ if(empty($_SESSION['cLogin'])){
     exit;
 }
 ;?>
-
+<div class="container">
+    <h1>Meus Anuncios - Adicionar Anuncio</h1>
+    <form method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="categoria">Categoria:</label>
+            <select name="categoria" id="categoria" class="form-control">
+                <option value=""></option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="titulo">Titulo:</label>
+            <input type="text" name="titulo" id="titulo" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="valor">Valor:</label>
+            <input type="text" name="valor" id="valor" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="descricao">Descrição:</label>
+            <textarea type="text" name="descricao" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="estado">Estado de Conservação:</label>
+                <select name="estado" id="estado" class="form-control">
+                    <option value="0">Ruim</option>
+                    <option value="1">Bom</option>
+                    <option value="2">Otimo</option>
+                </select>
+        </div>
+        <input type="submit" value="Adicionar" class="btn btn-default" />
+    </form>
+</div>
 
 <?php require 'pages/footer.php';?>
