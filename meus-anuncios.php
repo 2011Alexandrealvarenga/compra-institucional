09d7946a05e08495b8dcd90e41ffe40011bd00f4
2011brunofernandes <2011brunofernandes@gmail.com>
2021-10-12 15:14:43 -0300

    meus anuncios - correcoes

--- meus-anuncios.php
+++ meus-anuncios.php
@@ -1,6 +1,16 @@
 <?php require 'pages/header.php'; ?>
+<?php 
+// se nao estiver logado, direciona para a pagina de login
+if(empty($_SESSION['cLogin'])){
+    ?>
+    <script type="text/javascript">window.location.href="login.php";</script>
+    <?php 
+    exit;
+}
+;?>
 <div class="container">
     <h1>Meus Anuncios</h1>
+    <a href="add-anuncio.php" class="btn btn-default">Adicionar Anuncio</a>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -10,7 +20,20 @@
                 <th>AÇÕES</th>
             </tr>
         </thead>
-        <?php require 'class/anuncios.class.php' ;?>
+        <?php require 'classes/anuncios.class.php';
+        $a = new Anuncios();
+        // retorna o anuncios do usuario logado
+        $anuncios = $a->getMeusAnuncios();
+
+        foreach($anuncios as $anuncio):
+        ;?>
+        <tr>
+            <td><img src="assets/images/anuncios/<?php echo $anuncio['url']; ?>" border="0" /></td>
+            <td><?php echo $anuncio['titulo'] ;?> </td>
+            <td>R$ <?php echo number_format($anuncio['valor'], 2) ;?> </td>
+            <td></td>
+        </tr>
+        <?php endforeach; ?>
     </table>
 </div>
 
